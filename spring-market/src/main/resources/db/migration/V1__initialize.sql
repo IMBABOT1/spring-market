e5915cf67c6b3a7fe57b553ef1f768176ed3e970
unknown <zlovred12@gmail.com>
2022-06-28 18:04:52 +0300

    flyway

--- spring-market/src/main/resources/db/migration/V1__initialize.sql
+++ spring-market/src/main/resources/db/migration/V1__initialize.sql
@@ -1,4 +1,5 @@
-create table products (id bigserial primary key , title(varchar 255), price int);
+create table products (id bigserial primary key, title varchar(255), price int);
+
 insert into products(title, price)
 values
 ('demon souls', 10),
