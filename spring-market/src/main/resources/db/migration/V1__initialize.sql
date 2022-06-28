af6c064176a99855f575fe87bf3891dd39dec221
unknown <zlovred12@gmail.com>
2022-06-28 17:38:13 +0300

    Initial commit

---
+++ spring-market/src/main/resources/db/migration/V1__initialize.sql
@@ -0,0 +1,25 @@
+create table products (id bigserial primary key , title(varchar 255), price int);
+insert into products(title, price)
+values
+('demon souls', 10),
+('dark souls1', 20),
+('dark souls2', 25),
+('dark souls3', 30),
+('bloodborne', 35),
+('sekiro', 40),
+('elden ring', 50),
+('world of warcraft vanilla', 55),
+('world of warcraft burning crusade', 55),
+('world of warcraft wrath of the lich king', 55),
+('world of warcraft cataclysm', 55),
+('world of warcraft mists of pandaria', 55),
+('world of warcraft warlords of Draenor', 60),
+('world of warcraft legion', 60),
+('world of warcraft battle for azeroth', 60),
+('world of warcraft shadowlands', 60),
+('world of warcraft dragonflight', 60),
+('star craft 2  wings of liberty', 30),
+('star craft 2 heart of the swarm', 30),
+('star craft 2 legacy of the void', 30);
+
+
